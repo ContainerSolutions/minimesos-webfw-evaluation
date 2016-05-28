bdc5392f561607107c01520d595fa713b977ee58
Adam Sandor <adam.sandor@live.com>
2016-05-28 15:00:23 +0200

    Used memory measurement in Spark

--- spark/src/main/java/com/containersol/minivertx/HelloWorld.java
+++ spark/src/main/java/com/containersol/minivertx/HelloWorld.java
@@ -7,7 +7,10 @@ public class HelloWorld {
 		port(8080);
 		get("/hello", (request, response) -> {
 			response.header("Content-Type", "application/json");
-			return new HelloData("param1", "param2");
+			Runtime runtime = Runtime.getRuntime();
+			runtime.gc();
+			long usedMemory = runtime.totalMemory() - runtime.freeMemory();
+			return new HelloData("param1", "param2", usedMemory);
 		}, JsonUtil.json());
 	}
 }
